1da5dc3a2da0eb24c566dd79c8314c913a90d969
JorgeSolis <j.soliscontreras@ugto.mx>
2025-04-02 19:43:18 -0600

    [ADDED]: Ya se pueden agregar productos a la base de datos con el formulario, ademas agregue la funcion para obtener todos los productos en la base de datos

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,11 +1,34 @@
+import {addProduct, getProducts} from "./products.js"
+
 const openModal = document.querySelector(".openModalProduct")
 const closeModal = document.querySelector(".closeBtn")
 const modal = document.getElementById("modalNewProduct")
-
+const addProductForm = document.getElementById("newProductForm")
+// Funcion para abrir modal
 openModal.addEventListener("click", ()=>{
     modal.showModal()
 })
 
+// Funcion para cerrar modal
 closeModal.addEventListener("click", ()=>{
     modal.close()
 })
+
+addProductForm.addEventListener("submit", async(e)=>{
+    
+    e.preventDefault()
+    console.log("hola")
+    const img = document.getElementById("imageProduct").value
+    const name = document.getElementById("nameProduct").value
+    const category = document.getElementById("categoryProduct").value
+    const price = document.getElementById("priceProduct").value
+    const quantity = document.getElementById("quantityProduct").value
+    const date = document.getElementById("dateProduct").value
+
+    await addProduct(name, category, price, quantity, date, img )
+    modal.close()
+    addProductForm.reset()
+})
+
+getProducts()
+
