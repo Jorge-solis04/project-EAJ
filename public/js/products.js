253670cb580b74f8d1d5c08ab5f624fd4e583cc1
JorgeSolis <j.soliscontreras@ugto.mx>
2025-04-04 23:46:12 -0600

    [ADDED]: Agregue la tabla que se conecta a la base de datos, mostrando todos sus productos. Asimismo agregue la opcion de eliminarlos

--- public/js/products.js
+++ public/js/products.js
@@ -4,6 +4,7 @@ import {collection, addDoc, getDocs, updateDoc, deleteDoc, doc} from "https://ww
 
 const products = collection(db, "Productos")
 
+//Sirve para agregar productos a la base de datos
 export const addProduct = async(name, category, price, quantity, expiryDate, imgUrl) =>{
     console.log(name, category,price,quantity,expiryDate,imgUrl)
     await addDoc(products, {
@@ -12,14 +13,28 @@ export const addProduct = async(name, category, price, quantity, expiryDate, img
     
 }
 
+//Sirve para obtener todos los productos de la base de datos en JSON
 export const getProducts = async () =>{
     const allProducts = await getDocs(products)
 
     let arrayProducts = []
+
     allProducts.forEach((doc) => {
         arrayProducts.push({ id: doc.id, ...doc.data() })
     })
 
-    console.log(arrayProducts)
+    console.log("Base de datos:",arrayProducts)
     return arrayProducts
 }
+
+//Sirve para eliminar productos
+export const deleteProducts = async(id) =>{
+    const productsForDelete = doc(db, "Productos", id )
+    await deleteDoc(productsForDelete)
+}
+//Sirve para actualizar productos
+export const updateProduct = async (id, newData) => {
+    console.log(newData)
+    const product = doc (db, "usuarios", id)
+    await updateDoc(product, newData)
+} 
