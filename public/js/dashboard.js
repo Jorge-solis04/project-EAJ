56c92d60b5f09b5b92361e67fde82f8bddcf14cd
JorgeSolis <j.soliscontreras@ugto.mx>
2025-04-05 18:58:02 -0600

    [ADDED]: Boton para abrir detalles del producto

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -6,6 +6,7 @@ const openModal = document.querySelector(".openModalProduct")
 const closeModal = document.querySelector(".closeBtn")
 const modal = document.getElementById("modalNewProduct")
 const addProductForm = document.getElementById("newProductForm")
+const modalItemInfo = document.getElementById("modalItemInfo")
 
 onAuthStateChanged(auth, (user)=>{
     if (!user){
@@ -55,7 +56,7 @@ const loadProducts = async() =>{
          <td> ${item.price} </td>
          <td> ${item.quantity} </td>
          <td> ${item.expiryDate} </td>
-         <button class="btnDetails" item-id="${item.id}">More Details</button>
+         <button class="btnDetails" item-name="${item.name}" item-id="${item.id}"  item-category="${item.category}" item-expiryDate="${item.expiryDate}" item-price="${item.price}" item-quantity="${item.quantity}" item-imgUrl="${item.imgUrl}" >More Details</button>
          <button class="btnDelete" item-id="${item.id}">Delete </button>
         `
         productsTableBody.appendChild(row)
@@ -70,7 +71,50 @@ const loadProducts = async() =>{
             }
         })
     })
+
+    document.querySelectorAll(".btnDetails").forEach((btn) =>{
+        btn.addEventListener("click", (e) =>{
+            const id = e.target.getAttribute('item-id')
+            const name = e.target.getAttribute("item-name")
+            const category = e.target.getAttribute("item-category")
+            const expiryDate = e.target.getAttribute("item-expiryDate")
+            const price = e.target.getAttribute("item-price")
+            const quantity = e.target.getAttribute("item-quantity")
+            const img = e.target.getAttribute("item-imgUrl")
+            
+            const modalItem = document.createElement("dialog")
+            modalItem.classList.add("modalItem")
+            modalItem.innerHTML= ""
+
+            modalItem.innerHTML=
+            `
+                <div>
+                <h2> Details </h2>
+
+                <div>
+                    <img src= "${img}">
+                </div>
+
+                <h3> Product name: ${name} </h3>
+                <h4> Product id: ${id} </h4>
+                <h4> Category: ${category} </h4>
+                <h4> Expiry Date: ${expiryDate} </h4>
+                <h4> Price: $${price} </h4>
+                <h4> Quantity: ${quantity} </h4>
+                </div>
+                
+                <button class="closeItem" id="closeItem"> Discard </button>
+                <button class="closeItem" id="closeItem"> Edit </button>
+            `
+            modalItemInfo.appendChild(modalItem)
+
+            modalItem.showModal()
+
+            modalItem.querySelector(".closeItem").addEventListener("click", ()=>{
+                modalItem.close()
+                modalItem.remove();
+            })
+            
+        })
+    })
 } 
-
-
-
